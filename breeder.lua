eb2f6707c0af174a088d2d74bd2fa7b41a8b65e3
Jetpack_Maniac <rtf671@gmail.com>
2016-09-05 11:16:35 -0400

    Initial add for breeder.  Currently only has a description.

---
+++ breeder.lua
@@ -0,0 +1,17 @@
+
+
+-- #	Chromosome	Description
+-- 0	SPECIES	Species determines name, appearance, produce and preferred temperature and humidity for a bee. It also determines the genetic makeup a bee receives when it appears via mutation or as a world drop.
+-- 1	SPEED	Production speed
+-- 2	LIFESPAN	Lifespan
+-- 3	FERTILITY	The more fertile a bee is, the more drones a dying queen will spawn on average.
+-- 4	TEMPERATURE_TOLERANCE	Every bee has a preferred temperature. Only if the bee tolerates other temperatures than the preferred can she be transplanted to other climes.
+-- 5	NOCTURNAL	Nocturnal bees will work day and night
+-- 6	-	{Unused}
+-- 7	HUMIDITY_TOLERANCE	Climate is defined by temperature and humidity. This determines what other humidities the bee can tolerate apart from the preferred one.
+-- 8	TOLERANT_FLYER	Only strong or tolerant flyers can work in rain.
+-- 9	CAVE_DWELLING	Only cave dwellers will accept to be enclosed and not require a line of sight to the sky.
+-- 10	FLOWER_PROVIDER	Bees require flowers. Most just want a nice yellow or red flower in the vicinity, some will want something closer to home though (cacti, jungle plants) and a few have very exotic tastes.
+-- 11	FLOWERING	This determines the rate at which a bee will pollinate flowers around the apiary.
+-- 12	TERRITORY	Any bee will be protective of her territory, which also determines the area in which she'll search for flowers.
+-- 13	EFFECT	Several species have an effect on their environment which is determined by this chromosome.
