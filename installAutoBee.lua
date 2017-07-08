8f0ec890b68245afe42332799a164c1cf6804633
jetpack-maniac <rtf671@gmail.com>
2017-07-08 11:06:22 -0400

    Cleaned up installer

--- installAutoBee.lua
+++ installAutoBee.lua
@@ -1,31 +1,30 @@
 -- URLs
-local OC = "https://raw.githubusercontent.com/jetpack-maniac/autobee/master/autobee_oc.lua"
-
 computerCraftURLs ={
   autobee = "https://raw.githubusercontent.com/jetpack-maniac/autobee/master/autobee_cc.lua",
+  autobeeCore = "https://raw.githubusercontent.com/jetpack-maniac/autobee/master/autobeeCore.lua"
+}
+
+OpenComputersURLs = {
+  autobee = "https://raw.githubusercontent.com/jetpack-maniac/autobee/master/autobee_oc.lua",
   autobeeCore = "https://raw.githubusercontent.com/jetpack-maniac/autobee/master/autobeeCore.lua"
 }
 
 local issue = false
 
 if os.version ~= nil then -- This is a ComputerCraft OS API method
-  if os.version() == "CraftOS 1.8" then -- This is ComputerCraft for 1.9/1.10
+  if os.version() == "CraftOS 1.8" or os.version() == "CraftOS 1.7" then -- This is ComputerCraft for 1.9/1.10
     version = "ComputerCraft"
     term.clear()
     print("Welcome to the AutoBee Installer!")
     print("CraftOS 1.8 found.  Fetching AutoBee files...")
   end
-elseif pcall(function() computer.energy() end) == true then -- This is an OpenComputers Computer API method
+elseif pcall(function() computer = require("computer") computer.energy() end) == true then -- This is an OpenComputers Computer API method
   component = require("component")
   keyboard = require("keyboard")
   event = require("event")
   term = require("term")
   serialization = require("serialization")
   version = "OpenComputers"
-  print("Autobee Warning: OpenComputers does not yet support ComputerCraft APIs.  This program will not work on OpenComputers until it does.")
-  print("Continue installation anyway? (Y/n)")
-  os.exit()
-  -- End of temporary section
 end
 
 if version == "ComputerCraft" then
